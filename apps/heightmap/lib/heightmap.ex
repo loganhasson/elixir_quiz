2eb27181072ccec779feff392513183be907ee51
Logan Hasson <logan.hasson@gmail.com>
2015-10-25 00:42:32 -0400

    More setup

--- apps/heightmap/lib/heightmap.ex
+++ apps/heightmap/lib/heightmap.ex
@@ -1,2 +1,10 @@
 defmodule Heightmap do
+  def generate(size, :perlin), do: generate_perlin(size)
+  def generate(size, :diamond_square), do: generate_diamond_square(size)
+
+  def generate_perlin(size) do
+  end
+
+  def generate_diamond_square(size) do
+  end
 end
