cfa826565f8f5f708fa63222100169a659e62a96
Logan Hasson <logan.hasson@gmail.com>
2015-10-24 22:22:22 -0400

    Test fizzbuzz

--- apps/elixir_fizzbuzz/lib/elixir_fizzbuzz.ex
+++ apps/elixir_fizzbuzz/lib/elixir_fizzbuzz.ex
@@ -1,6 +1,17 @@
 defmodule ElixirFizzbuzz do
+  @moduledoc """
+  Classic FizzBuzz.
+  """
+  @doc """
+  Returns a joined binary of FizzBuzz up to a given limit.
+
+  ## Examples
+
+      iex> ElixirFizzbuzz.run(16)
+      "1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz 16"
+  """
   def run(upper_limit \\ 100) do
-    1..100
+    1..upper_limit
     |> Stream.map(&fizz/1)
     |> Enum.join(" ")
   end
--- apps/elixir_fizzbuzz/test/elixir_fizzbuzz_test.exs
+++ apps/elixir_fizzbuzz/test/elixir_fizzbuzz_test.exs
@@ -1,7 +1,5 @@
 defmodule ElixirFizzbuzzTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
 
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
+  doctest ElixirFizzbuzz
 end
